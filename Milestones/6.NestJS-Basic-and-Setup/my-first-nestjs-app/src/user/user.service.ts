5869f12cc0622ca696ef637f1c6e4484f3cb53c3
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2025-09-03 16:48:57 +1000

    chore: remove exist user apis

--- Milestones/6.NestJS-Basic-and-Setup/my-first-nestjs-app/src/user/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class UserService {
-  private users = ['Alice', 'Bob', 'John'];
-  getUsers() {
-    return this.users;
-  }
-}
